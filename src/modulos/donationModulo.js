2ddd3942eaf21cfc65497409c4ca384bfef140a4
Lucas Ternes <lucasternes11@gmail.com>
2026-08-25 22:02:41 -0300

    Remove donationStore module and its functionality

--- src/modulos/donationModulo.js
+++ src/modulos/donationModulo.js
@@ -1,11 +1,1 @@
-let donations = []
 
-export const donationStore = {
-    initialize(initialDonations = []) {
-        donations = [...initialDonations]
-    },
-
-    get() {
-        return [...donations]
-    }
-}
